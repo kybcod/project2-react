df699626f8d628d324ce2a6ebf17a0334bae6f6b
kyb <kyb-0215@naver.com>
2024-05-21 11:24:12 +0900

    navbar 작성

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,13 @@
 import { Box } from "@chakra-ui/react";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import React from "react";
+import { Navbar } from "../component/Navbar.jsx";
 
 export function Home() {
   return (
     <Box>
       <Box>
-        <Link to={"/"}>Home</Link>
-        <Link to={"write"}>글 작성</Link>
+        <Navbar />
       </Box>
       <Box>
         <Outlet />
