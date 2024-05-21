5d67c83f2d32359d9a37eb045b362b0b0864a8f5
kyb <kyb-0215@naver.com>
2024-05-21 15:11:49 +0900

    board null 값 처리 {}

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -1,11 +1,47 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import { Box, FormControl, FormLabel, Input } from "@chakra-ui/react";
 
 export function BoardView() {
   const { id } = useParams();
+  const [board, setBoard] = useState({});
+
   useEffect(() => {
-    axios.get(`/api/board/${id}`);
+    axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
   }, []);
-  return null;
+
+  // if (board === null) {
+  //   return <Spinner />;
+  // }
+
+  return (
+    <Box>
+      <Box>{board.id}번 게시물</Box>
+      <Box>
+        <FormControl>
+          <FormLabel>제목</FormLabel>
+          <Input value={board.title} readOnly />
+        </FormControl>
+        <Box>
+          <FormControl>
+            <FormLabel>본문</FormLabel>
+            <Input value={board.content} readOnly />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>작성자</FormLabel>
+            <Input value={board.writer} readOnly />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>작성일시</FormLabel>
+            <Input type={"datetime-local"} value={board.inserted} readOnly />
+          </FormControl>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
