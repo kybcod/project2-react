6a7f0ef980280acf180e87790d42158517c8de7c
kyb <kyb-0215@naver.com>
2024-05-22 09:19:55 +0900

    회원가입 화면, 네브바 설정

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { Home } from "./page/Home.jsx";
 import { BoardWrite } from "./page/board/BoardWrite.jsx";
 import { BoardList } from "./page/board/BoardList.jsx";
 import { BoardView } from "./page/board/BoardView.jsx";
-import { BoardEdit } from "./BoardEdit.jsx";
+import { BoardEdit } from "./page/board/BoardEdit.jsx";
+import { MemberSignup } from "./page/member/MemberSignup.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
       { path: "write", element: <BoardWrite /> },
       { path: "board/:id", element: <BoardView /> },
       { path: "edit/:id", element: <BoardEdit /> },
+      { path: "signup", element: <MemberSignup /> },
     ],
   },
 ]);
