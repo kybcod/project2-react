bde8c48c6cde6b59805484ad1272cbd5cbd0d9eb
kyb <kyb-0215@naver.com>
2024-05-23 16:01:09 +0900

    로그인한 사용자 닉네임 네브바에 보이기

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import { Box, Button, ButtonGroup, Flex, Spacer } from "@chakra-ui/react";
 import React, { useContext } from "react";
 import { LoginContext } from "./LoginProvider.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
 
 export function Navbar() {
   const navigate = useNavigate();
@@ -16,48 +18,67 @@ export function Navbar() {
       >
         Home
       </Box>
-      <Box
-        onClick={() => navigate("/write")}
-        cursor={"pointer"}
-        _hover={{ bgColor: "gray.200" }}
-      >
-        글쓰기
-      </Box>
-      <Box
-        onClick={() => navigate("/signup")}
-        cursor={"pointer"}
-        _hover={{ bgColor: "gray.200" }}
-      >
-        회원가입
-      </Box>
-      <Box
-        onClick={() => navigate("/member/list")}
-        cursor={"pointer"}
-        _hover={{ bgColor: "gray.200" }}
-      >
-        회원목록
-      </Box>
+      {account.isLoggedIn() && (
+        <Box
+          onClick={() => navigate("/write")}
+          cursor={"pointer"}
+          _hover={{ bgColor: "gray.200" }}
+        >
+          글쓰기
+        </Box>
+      )}
+      <Spacer />
+      {account.isLoggedIn() && (
+        <Box>
+          <FontAwesomeIcon icon={faUser} />
+          {account.nickName}
+        </Box>
+      )}
+      {account.isLoggedIn() || (
+        <Box
+          onClick={() => navigate("/member/list")}
+          cursor={"pointer"}
+          _hover={{ bgColor: "gray.200" }}
+        >
+          회원목록
+        </Box>
+      )}
+
       <Spacer />
       <ButtonGroup gap="1">
-        <Button
-          onClick={() => navigate("/login")}
-          cursor={"pointer"}
-          _hover={{ bgColor: "gray.200" }}
-          colorScheme="teal"
-        >
-          로그인
-        </Button>
-        <Button
-          onClick={() => {
-            account.logout();
-            navigate("/login");
-          }}
-          cursor={"pointer"}
-          _hover={{ bgColor: "gray.200" }}
-          colorScheme="teal"
-        >
-          로그아웃
-        </Button>
+        {account.isLoggedIn() || (
+          <Button
+            onClick={() => navigate("/signup")}
+            cursor={"pointer"}
+            _hover={{ bgColor: "gray.200" }}
+            colorScheme="teal"
+          >
+            회원가입
+          </Button>
+        )}
+        {account.isLoggedIn() || (
+          <Button
+            onClick={() => navigate("/login")}
+            cursor={"pointer"}
+            _hover={{ bgColor: "gray.200" }}
+            colorScheme="teal"
+          >
+            로그인
+          </Button>
+        )}
+        {account.isLoggedIn() && (
+          <Button
+            onClick={() => {
+              account.logout();
+              navigate("/login");
+            }}
+            cursor={"pointer"}
+            _hover={{ bgColor: "gray.200" }}
+            colorScheme="teal"
+          >
+            로그아웃
+          </Button>
+        )}
       </ButtonGroup>
     </Flex>
   );
