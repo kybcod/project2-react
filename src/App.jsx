5611502a0e8046b67868e01fb8cde6aa632d1acc
kyb <kyb-0215@naver.com>
2024-05-24 14:10:46 +0900

    axios interceptor 설정 token 있을 때만 Authorization 헤더 추가

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,16 @@ import { MemberView } from "./page/member/MemberView.jsx";
 import { MemberEdit } from "./page/member/MemberEdit.jsx";
 import { MemberLogin } from "./page/member/MemberLogin.jsx";
 import { LoginProvider } from "./component/LoginProvider.jsx";
+import axios from "axios";
+
+// axios interceptor 설정
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 const router = createBrowserRouter([
   {
