1316f7e93ed100124997cc4f7a9ed658eaabe8e5
kyb <kyb-0215@naver.com>
2024-05-27 17:50:42 +0900

    검색 기능 구현

--- src/page/member/MemberList.jsx
+++ src/page/member/MemberList.jsx
@@ -2,6 +2,9 @@ import {
   Box,
   Button,
   Center,
+  Flex,
+  Input,
+  Select,
   Spinner,
   Table,
   Tbody,
@@ -19,12 +22,16 @@ export function MemberList() {
   const [memberPageInfo, setMemberPageInfo] = useState({});
   const navigate = useNavigate();
   const [page] = useSearchParams();
+  const [type, setType] = useState("all");
+  const [keyword, setKeyword] = useState("");
 
   useEffect(() => {
     axios.get(`/api/member/list?${page}`).then((res) => {
       setMemberList(res.data.memberList);
       setMemberPageInfo(res.data.memberPageInfo);
     });
+    const typeParam = page.get("type");
+    const keywordParam = page.get("keyword");
   }, [page]);
 
   const pageNumbers = [];
@@ -36,69 +43,90 @@ export function MemberList() {
     return <Spinner />;
   }
 
+  function handlePageClick(pageNumber) {
+    page.set("page", pageNumber);
+    navigate(`?${page}`);
+  }
+
   return (
     <Box mt={"30px"}>
       <Box>
-        <Table>
-          <Thead>
-            <Tr>
-              <Th>No</Th>
-              <Th>이메일</Th>
-              <Th>닉네임</Th>
-              <Th>가입일시</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {memberList.map((member) => (
-              <Tr
-                key={member.id}
-                cursor={"pointer"}
-                _hover={{ bgColor: "gray.200" }}
-                onClick={() => navigate(`/member/${member.id}`)}
-              >
-                <Td>{member.id}</Td>
-                <Td>{member.email}</Td>
-                <Td>{member.nickName}</Td>
-                <Td>{member.signupDateAndTime}</Td>
+        {memberList.length === 0 && (
+          <Center>해당 회원이 존재하지 않습니다.</Center>
+        )}
+        {memberList.length > 0 && (
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>No</Th>
+                <Th>이메일</Th>
+                <Th>닉네임</Th>
+                <Th>가입일시</Th>
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
+            </Thead>
+            <Tbody>
+              {memberList.map((member) => (
+                <Tr
+                  key={member.id}
+                  cursor={"pointer"}
+                  _hover={{ bgColor: "gray.200" }}
+                  onClick={() => navigate(`/member/${member.id}`)}
+                >
+                  <Td>{member.id}</Td>
+                  <Td>{member.email}</Td>
+                  <Td>{member.nickName}</Td>
+                  <Td>{member.signupDateAndTime}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        )}
       </Box>
 
-      {/*/!*검색*!/*/}
-      {/*<Box>*/}
-      {/*  <Flex>*/}
-      {/*    <Box>*/}
-      {/*      <Select>*/}
-      {/*        <option value={"all"}>전체</option>*/}
-      {/*        <option value={"text"}>글(제목+내용)</option>*/}
-      {/*        <option value={"nickName"}> 작성자</option>*/}
-      {/*      </Select>*/}
-      {/*    </Box>*/}
-      {/*    <Box></Box>*/}
-      {/*    <Box></Box>*/}
-      {/*  </Flex>*/}
-      {/*</Box>*/}
+      {/*검색*/}
+      <Center mt={"30px"}>
+        <Box>
+          <Flex>
+            <Box>
+              <Select onChange={(e) => setType(e.target.value)}>
+                <option value={"all"}>전체</option>
+                <option value={"email"}>이메일</option>
+                <option value={"nickName"}>닉네임</option>
+              </Select>
+            </Box>
+            <Box>
+              <Input onChange={(e) => setKeyword(e.target.value)} />
+            </Box>
+            <Box>
+              <Button
+                onClick={() => navigate(`?type=${type}&keyword=${keyword}`)}
+              >
+                검색
+              </Button>
+            </Box>
+          </Flex>
+        </Box>
+      </Center>
 
       {/*/!*페이지네이션*!/*/}
       <Center>
-        {memberPageInfo.prevPage && (
-          <>
-            <Button onClick={() => navigate(`?page=1`)}>처음</Button>
-            <Button
-              onClick={() => navigate(`?page=${memberPageInfo.prevPage}`)}
-            >
-              이전
-            </Button>
-          </>
-        )}
-        <Box>
+        <Box mt={"30px"}>
+          {memberPageInfo.prevPage && (
+            <>
+              <Button onClick={() => navigate(`?page=1`)}>처음</Button>
+              <Button
+                onClick={() => navigate(`?page=${memberPageInfo.prevPage}`)}
+              >
+                이전
+              </Button>
+            </>
+          )}
+
           {pageNumbers.map((pageNumber) => (
             <Button
               mr={"10px"}
               onClick={() => {
-                navigate(`?page=${pageNumber}`);
+                handlePageClick(pageNumber);
               }}
               key={pageNumber}
               colorScheme={
@@ -108,6 +136,7 @@ export function MemberList() {
               {pageNumber}
             </Button>
           ))}
+
           {memberPageInfo.nextPage && (
             <>
               <Button
