03d53e074a7e55d86e971cd168e8c9dc825019f9
kyb <kyb-0215@naver.com>
2024-05-27 09:42:56 +0900

    페이지 의존성 주입하여 화면 변경

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -1,9 +1,9 @@
-import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPen } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export function BoardList() {
   const navigate = useNavigate();
@@ -14,10 +14,13 @@ export function BoardList() {
   //   { id: 7, title: "title1", writer: "who1" },
   //   { id: 8, title: "title1", writer: "who1" },
   // ];
+  const [searchParams] = useSearchParams(); //key,value로 저장
 
   useEffect(() => {
-    axios.get("/api/board/list").then((res) => setBoardList(res.data));
-  }, []);
+    axios
+      .get(`/api/board/list?${searchParams}`)
+      .then((res) => setBoardList(res.data));
+  }, [searchParams]); //의존성(dependency)가 있다면 변경될때마다 함수를 trigger를 합니다.
 
   return (
     <Box mt={"30px"}>
@@ -48,6 +51,16 @@ export function BoardList() {
             ))}
           </Tbody>
         </Table>
+      </Box>
+      <Box>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((pageNumber) => (
+          <Button
+            onClick={() => navigate(`/?page=${pageNumber}`)}
+            key={pageNumber}
+          >
+            {pageNumber}
+          </Button>
+        ))}
       </Box>
     </Box>
   );
