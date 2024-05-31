8e1f2ab55414a3cdec6511fafa4d5a90a9e3f81b
kyb <kyb-0215@naver.com>
2024-05-31 11:23:27 +0900

    댓글 수정 완료

--- src/component/Comment/CommentEdit.jsx
+++ src/component/Comment/CommentEdit.jsx
@@ -1,4 +1,17 @@
-import { Box, Button, Flex, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Textarea,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
@@ -12,16 +25,30 @@ export function CommentEdit({
   setIsEditing,
 }) {
   const [commentText, setCommentText] = useState(comment.comment);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
 
   function handleCommentSubmit() {
+    setIsProcessing(true);
     axios
       .put("/api/comment/edit", {
         id: comment.id,
         comment: commentText,
       })
-      .then((res) => {})
+      .then((res) => {
+        toast({
+          description: "수정되었습니다.",
+          position: "top",
+          status: "success",
+          duration: 1000,
+        });
+      })
       .catch()
-      .finally();
+      .finally(() => {
+        onClose();
+        setIsProcessing(false);
+        setIsEditing(false);
+      });
   }
 
   return (
@@ -41,13 +68,25 @@ export function CommentEdit({
           <FontAwesomeIcon icon={faXmark} />
         </Button>
         <Button
-          onClick={handleCommentSubmit}
+          isLoading={isProcessing}
+          onClick={onOpen}
           variant={"outline"}
           colorScheme={"blue"}
         >
           <FontAwesomeIcon icon={faPaperPlane} />
         </Button>
       </Box>
+      <Modal isOpen={isOpen} onClose={onclose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>수정 확인</ModalHeader>
+          <ModalBody>정말로 수정하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={handleCommentSubmit}>확인</Button>
+            <Button onClick={onClose}>취소</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
